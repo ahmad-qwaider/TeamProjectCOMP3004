d05f180748e740c58497a98ace61c1a47946ad03
ridhwanmcarleton <165201344+ridhwanmcarleton@users.noreply.github.com>
2024-04-08 17:56:49 -0400

    Update Electrode.cc This version doesnt use wave object. This is base simple version

--- Electrode.cc
+++ Electrode.cc
@@ -1,1 +1,22 @@
+#include "electrode.h"
 
+Electrode::Electrode(int pos, string& name, int freq)
+{
+    position = pos;
+    waveType = name;
+    signalStrength = freq;
+}
+Electrode::~Electrode() {}
+
+int Electrode::emitTherapy(){
+    cout << "electrode at position " << position << " emits " << waveType << " wave of fequency " << signalStrength << " Hz." << endl;
+    return signalStrength;
+}
+
+void Electrode::addOffset(){
+    signalStrength += 5;
+}
+
+void Electrode::resetOffset(){
+    signalStrength -= 20;
+}
