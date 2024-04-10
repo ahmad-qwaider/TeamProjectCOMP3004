d1f347df6e820b15b4e76acb7be120252cb3836b
ridhwanmcarleton <165201344+ridhwanmcarleton@users.noreply.github.com>
2024-04-10 19:27:46 -0400

    Create PC.h incomplete

---
+++ PC.h
@@ -0,0 +1,18 @@
+#ifndef PC_H
+#define PC_H
+#include "Session.h"
+#include <QVector>
+#include <QDebug>
+
+class PC
+{
+    public:
+        PC();
+        void upload(const QVector<SessionData> &sessionsToUpload);
+        void viewHistory();
+    private:
+        QVector<SessionData> sessions;
+
+};
+
+#endif // PC_H
