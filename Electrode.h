17da67753310c925584ee590b4e2a97187ced970
ridhwanmcarleton <165201344+ridhwanmcarleton@users.noreply.github.com>
2024-04-17 19:59:43 -0400

    Update Electrode.h

--- Electrode.h
+++ Electrode.h
@@ -8,6 +8,19 @@
 using namespace std;
 /* Purpose of Class: To act as electrode object
  * Data Members:
+ * bool isActive - status of electrode, help confirm connection
+ * int position - location
+ * int f1,f1,f2,f3,a1,a2,a3 - sin wave elements of current electrodes "eeg waveform". f is frequncy and a is ampliture. 3 sets
+ *
+ * Class Functions:
+ *  QVector<int> emitSignal() - simulate electrode giving off certain wave form via printing out its atributes, returns a integer QVector with wave elements
+ *                              in the following order f1 f2 f3 a1 a2 a3.
+ *  QVector<double> getWaveData(int durationInSeconds, int sampleRate) - gets wave data for graphing
+ *  void treatmentAffect() - simulate the affect of treatment by incrementing frequency
+ *  void treat(int Fd, int offSet) - simulate treatment by printing to console
+ *  void switchIsActive() - swap is active to oposite
+ *
+ *  Getters and Setters
  */
 
 class Electrode 
